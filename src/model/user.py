94cdb23e18bfddb00a37e5c893e79214dd1a9da7
Pakorn Ueareeworakul <N1ghtZerst0rer@gmail.com>
2019-01-19 17:55:28 +0800

    Add model for user

---
+++ src/model/user.py
@@ -0,0 +1,17 @@
+class User:
+    currentUser = None
+    
+    def __init__(self, name, token):
+        self.name = name
+        self.token = token
+
+    @staticmethod
+    def register(name, userid):
+        # do a server request
+        User.currentUser = User(name, userid)
+
+    @staticmethod
+    def login(userid):
+        # do a server request
+        # dummy for testing
+        User.currentUser = User("test", userid)
