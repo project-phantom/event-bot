3f171f555fef3e8a9ecc6307997ad782bfb42094
zzz686970 <1564256031@qq.com>
2019-01-20 05:43:25 +0800

    fix import error

---
+++ calendar_telegram/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: zzz686970
+# @Date:   2019-01-20 05:34:35
+# @Last Modified by:   zzz686970
+# @Last Modified time: 2019-01-20 05:40:38
+# __import__('calendar-telegram-master')
+from calendar_telegram import telegramcalendar
+from calendar_telegram import telegramoptions
